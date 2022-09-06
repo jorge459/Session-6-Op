5c9699ee820aa0d63558678a4afb72266844f4f8
Jorge Flores <jorgef2465@gmail.com>
2022-09-06 14:41:27 -0500

    Inicializando ejercios de la session 6

---
+++ src/main/java/com/jachi/ejercicios/session6/Session6Application.java
@@ -0,0 +1,13 @@
+package com.jachi.ejercicios.session6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Session6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jachi/ejercicios/session6/Session6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jachi.ejercicios.session6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
