21015528f58c2ebf7b8371e1c4fe63242d498100
Jorge Flores <jorgef2465@gmail.com>
2022-09-06 15:07:53 -0500

    Agregando las definiciones del Controlador LaptopController y agregando datos iniciales en la clase main

--- src/main/java/com/jachi/ejercicios/session6/Session6Application.java
+++ src/main/java/com/jachi/ejercicios/session6/Session6Application.java
@@ -1,13 +1,19 @@
 package com.jachi.ejercicios.session6;
 
+import com.jachi.ejercicios.session6.models.Laptop;
+import com.jachi.ejercicios.session6.repositories.LaptopRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Session6Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Session6Application.class, args);
+		ApplicationContext context = SpringApplication.run(Session6Application.class, args);
+		LaptopRepository repository = context.getBean(LaptopRepository.class);
+		Laptop laptop= new Laptop(null,"Toshiba","Z-400",16,3500D);
+		repository.save(laptop);
 	}
 
 }
---
+++ src/main/java/com/jachi/ejercicios/session6/controllers/LaptopController.java
@@ -0,0 +1,20 @@
+package com.jachi.ejercicios.session6.controllers;
+
+import com.jachi.ejercicios.session6.models.Laptop;
+import com.jachi.ejercicios.session6.repositories.LaptopRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LaptopController {
+    private LaptopRepository repository;
+    public LaptopController(LaptopRepository repository){
+        this.repository = repository;
+    }
+    @GetMapping("/laptops")
+    public List<Laptop> findAll(){
+        return repository.findAll();
+    }
+}
